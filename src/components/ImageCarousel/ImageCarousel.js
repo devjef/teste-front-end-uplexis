d77a1de0bfe905ece0a3ad2486fea273e7df5ae3
Jeferson <jeferson.ol.lopes@gmail.com>
2020-12-28 12:14:08 -0300

    carousel responsivo

--- src/components/ImageCarousel/ImageCarousel.js
+++ src/components/ImageCarousel/ImageCarousel.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import { CarouselProvider, Slider, Slide } from 'pure-react-carousel';
 import 'pure-react-carousel/dist/react-carousel.es.css';
 import img1 from '../../assets/img/in-1.png';
 import img2 from '../../assets/img/in-2.png';
@@ -7,21 +7,20 @@ import img2 from '../../assets/img/in-2.png';
 function ImageCarousel() {
 
     return (
-        <div className="carousel-image">
-            <CarouselProvider isIntrinsicHeight={true}
-                naturalSlideHeight={30}
-                totalSlides={4}
-                isPlaying={true}
-                visibleSlides={2}
-                playDirection={'forward'}
-                interval={2000}
+        <div className="carousel">
+            <CarouselProvider 
+            visibleSlides={2}
+            totalSlides={4}
+            naturalSlideWidth={400}
+            isIntrinsicHeight={true}
+            currentSlide={0}
+            isPlaying
                 >
-                
                 <Slider>
-                    <Slide index={0}><img src={img1} alt="Imagem carrousel" /></Slide>
-                    <Slide index={1}><img src={img2} alt="Imagem carrousel" /></Slide>
-                    <Slide index={2}><img src={img1} alt="Imagem carrousel" /></Slide>
-                    <Slide index={3}><img src={img2} alt="Imagem carrousel" /></Slide>
+                    <Slide index={0}><img className="carousel__img" src={img1} alt="Imagem carrousel" /></Slide>
+                    <Slide index={1}><img className="carousel__img" src={img2} alt="Imagem carrousel" /></Slide>
+                    <Slide index={2}><img className="carousel__img" src={img1} alt="Imagem carrousel" /></Slide>
+                    <Slide index={3}><img className="carousel__img" src={img2} alt="Imagem carrousel" /></Slide>
                 </Slider>
             </CarouselProvider>
         </div>
